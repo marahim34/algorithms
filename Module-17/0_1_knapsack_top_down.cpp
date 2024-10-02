40679556ab4146604c34789e119c4ea3201e17ec
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-10-03 01:11:50 +0300

    0-1 knapsack top-bottom approach

---
+++ Module-17/0_1_knapsack_top_down.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxN = 1000;
+const int maxW = 1000;
+int dp[maxN][maxW];
+
+int knapsack(int n, int weight[], int value[], int W)
+{
+    if (n < 0 || W == 0)
+        return 0;
+
+    if (dp[n][W] == -1)
+    {
+        return dp[n][W];
+    }
+
+    if (weight[n] <= W)
+    {
+        // Two options: either we have to take it or consider it without taking it
+        // দুটি বিকল্প: হয় নিয়ে দেখতে হবে বা না নিয়ে দেখতে হবে
+        int opt1 = knapsack(n - 1, weight, value, W - weight[n]) + value[n];
+        int opt2 = knapsack(n - 1, weight, value, W);
+
+        return dp[n][W] = max(opt1, opt2);
+    }
+    else
+    {
+        // One option
+        // We have to consider it without taking it
+        // একটি বিকল্প
+        // না নিয়ে দেখতে হবে
+        int opt2 = knapsack(n - 1, weight, value, W);
+        return opt2;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int weight[n], value[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> weight[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> value[i];
+    }
+
+    int W;
+    cin >> W;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
+
+    cout << knapsack(n - 1, weight, value, W);
+
+    return 0;
+}
