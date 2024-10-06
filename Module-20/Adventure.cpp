7596d832bd6c899ff460d3998bca3efc58b8aec7
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-10-07 01:32:10 +0300

    bfs and equal sum implemented for contest problems

--- Module-20/Adventure.cpp
+++ Module-20/Adventure.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int maxN = 1e3 + 5;
+const int maxW = 1e5 + 5;
+
+int dp[maxN][maxW];
+
 int knapsack(int n, int weight[], int val[], int W)
 {
     if (n < 0 || W < 0)
@@ -8,16 +13,24 @@ int knapsack(int n, int weight[], int val[], int W)
         return 0;
     }
 
+    if (dp[n][W] != -1)
+    {
+        return dp[n][W];
+    }
+    else
+    {
+    }
+
     if (weight[n] <= W)
     {
         int opt1 = knapsack(n - 1, weight, val, W - weight[n]) + val[n];
         int opt2 = knapsack(n - 1, weight, val, W);
 
-        return max(opt1, opt2);
+        return dp[n][W] = max(opt1, opt2);
     }
     else
     {
-        return knapsack(n - 1, weight, val, W);
+        return dp[n][W] = knapsack(n - 1, weight, val, W);
     }
 }
 
@@ -41,6 +54,14 @@ int main()
         for (int i = 0; i < n; i++)
         {
             cin >> val[i];
+        }
+
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= W; j++)
+            {
+                dp[i][j] = -1;
+            }
         }
 
         int ans = knapsack(n - 1, weight, val, W);
